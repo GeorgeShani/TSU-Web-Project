c29eef0b3446b269f6ad49a44b43898d2e8ec718
GeorgeShani <shanidze.geo2004@gmail.com>
2025-06-23 22:54:57 +0400

    Update Track Querying in Index.php

--- index.php
+++ index.php
@@ -4,13 +4,22 @@
   $stmt = $pdo->prepare("
     SELECT 
       tracks.*,
-      GROUP_CONCAT(DISTINCT all_artists.stage_name ORDER BY all_artists.stage_name SEPARATOR ', ') AS all_artists
+      GROUP_CONCAT(
+        DISTINCT artists.stage_name
+        ORDER BY 
+          CASE 
+            WHEN artists.id = main_artist.id THEN 0 
+            ELSE 1 
+          END,
+          artists.stage_name
+        SEPARATOR ', '
+      ) AS all_artists
+
     FROM tracks
     JOIN artists AS main_artist ON tracks.artist_id = main_artist.id
+
     LEFT JOIN track_features ON track_features.track_id = tracks.id
-    LEFT JOIN artists AS featured_artist ON track_features.artist_id = featured_artist.id
-    LEFT JOIN (SELECT id, stage_name FROM artists) AS all_artists 
-    ON all_artists.id = tracks.artist_id OR all_artists.id = track_features.artist_id
+    LEFT JOIN artists ON artists.id = track_features.artist_id OR artists.id = tracks.artist_id
     WHERE tracks.album_id IS NULL
     GROUP BY tracks.id
   ");
